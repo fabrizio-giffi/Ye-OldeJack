5c26ba6c90c390365e4636ae27d93e20e310fb75
Fabrizio Giffi <fabrizio.giffi@gmail.com>
2023-01-24 19:11:07 +0100

    Second day. Styling and HTML structure. Betting logic. Bugfixes. Game screen needs styling.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // DOM elements
 const splashScreen = document.querySelector("#splash-screen")
-const startBtn = splashScreen.querySelector("button")
+const startBtn = document.querySelector("#start-game-btn")
+
+const betScreen = document.querySelector("#bet-screen")
+
 const gameScreen = document.querySelector("#game-screen")
 const hitBtn = document.querySelector("#hit")
 const standBtn = document.querySelector("#stand")
+
 const dealerHandUL = document.querySelector("#dealer-hand")
 const playerHandUL = document.querySelector("#player-hand")
-const playerScore = document.querySelector("aside")
-const playerScoreValue = document.querySelector("#player-score")
+
+const playerScoreAside = document.querySelector("#player-score-aside")
+const playerScoreValue = playerScoreAside.querySelector("span")
+
+const betMoneyAside = document.querySelector("#bet-money-aside")
+const betMoneyPile = document.querySelector("#bet-money-pile")
+
+const moneyPlayerAside = document.querySelector("#money-player-aside")
+const moneyPlayerImg = document.querySelector("#money-player-img")
+
+const hiddenGame = document.querySelectorAll(".ctnHidden")
+const betMiddleAside = document.querySelector("#bet-middle-aside")
+const betMiddleImg = document.querySelector("#bet-middle-img")
+
+const playerBtn = document.querySelector("#btnHidden")
+
 
 // Full deck of cards - 52 cards - 4 Suits
 // Hearts, Diamons, Clubs, Spades
@@ -70,28 +88,28 @@ class Game {
       ]
   
       this.removedDeck = []
-  
+      this.moneyDisplay = 0
+      this.totalBet = 0
+
       this.player = {
         Hand : [],
+        Bet : 0,
         Money : 500,
         cardsCount: 0,
-        Bet : 0,
         Go : true,
         canDraw : true,
       };
   
       this.dealer = {
         Hand : [],
+        Bet : 0,
         Money : 2500,
         cardsCount: 0,
-        Bet : 0,
         canDraw : true,
-        // Go : false,
       }
 
-      // Initializing some conditions
+      // Initializing some variables
       this.isBlackjack = false
-      playerScore.style.display = "none"
   }
   // END Constructor
 
@@ -100,6 +118,25 @@ class Game {
     showScore(hand) {
       let score = hand.reduce((acc, current) => acc + current.value, 0)
       return score
+    }
+
+    showMoney(money) {
+      if(money > 200) {
+        this.moneyDisplay = 500;
+      }
+      else if (money <= 200 && money > 50) {
+        this.moneyDisplay = 200;
+      }
+      else if (money <= 50 && money > 20) {
+        this.moneyDisplay = 50;
+      }
+      else if (money <= 20 && money > 10) {
+        this.moneyDisplay = 20;
+      }
+      else {
+        this.moneyDisplay = 10
+      }
+      return "./images/assets/"+this.moneyDisplay+".png"
     }
 
     drawCard() {
@@ -137,37 +174,46 @@ class Game {
 
     checkBlackjack() {
       if (this.showScore(this.player.Hand) === 21 && this.showScore(this.dealer.Hand) < 21) {
-        console.log("Blackjack! You won!")
         return "Blackjack! You won!"
       }
       else if(this.showScore(this.dealer.Hand) === 21 && this.showScore(this.player.Hand) < 21){
-        console.log("Blackjack! The dealer wins!")
         return "Blackjack! The dealer wins!"
       }
       else if(this.showScore(this.player.Hand) === 21 && this.showScore(this.player.Hand) === this.showScore(this.dealer.Hand)) {
-        console.log("What are the odds! A Blackjack tie!")
         return "What are the odds! A Blackjack tie!"
       }
     }
     
+    playerBet() {
+      betMoneyPile.setAttribute("src", this.showMoney(this.player.Money))
+      betMoneyAside.firstChild.innerText = "Your money: " + this.player.Money + "¢"
+
+      const betInput = document.querySelector("#bet-input")
+      const betOutput = document.querySelector("#bet-output")
+      const betBtn = document.querySelector("#bet-btn")
+      
+      betInput.setAttribute("max", this.player.Money)
+      betOutput.innerText = betInput.value
+
+      // Event listener to display range value
+      betInput.addEventListener("input", (event) => {
+        betOutput.innerText = event.target.value
+      })
+      // Event listener to place the bet
+      betBtn.onclick = () => {
+        this.player.Money -= betInput.value
+        this.player.Bet = betInput.value
+        this.dealer.Bet = this.player.Bet
+        this.totalBet = Number(this.player.Bet) + Number(this.dealer.Bet)
+
+        betScreen.style.display = "none"
+        gameScreen.style.display = "flex"
+        playerBtn.style.display = "flex"
+        this.playerTurn()
+      }
+    }
+
     playerTurn() {
-
-      // TESTING
-      standBtn.onclick = () => {
-        this.player.Go = false
-      }
-      hitBtn.onclick = () => {
-        this.drawCard();
-        console.log("clicking")
-      }
-    }
-
-    shuffleDeck() {
-      this.removedDeck.forEach(card => this.cardsDeck.push(card))
-    }
-    
-
-    startGame() {
       let delay = 300
       setTimeout(() => {
         this.drawCard()
@@ -184,27 +230,43 @@ class Game {
         this.player.Go = true
       }, delay*4);
       setTimeout(() => {
-        playerScore.style.display = "block"
+        hiddenGame.forEach(element => element.style.display = "flex" )
+        console.log(moneyPlayerAside)
+        moneyPlayerAside.firstChild.innerText = this.player.Money + "¢"
+        moneyPlayerImg.setAttribute("src", this.showMoney(this.player.Money))
+        betMiddleAside.firstChild.innerText = "Total bet: " + (Number(this.player.Bet) + Number(this.dealer.Bet)) + "¢"
+        betMiddleImg.setAttribute("src", this.showMoney(this.totalBet))
+        this.checkBlackjack()
       }, delay*5);
-      
-    
-    this.checkBlackjack()
-    this.playerTurn()
+
+
+      // TESTING
+      standBtn.onclick = () => {
+        this.player.Go = false
+      }
+      hitBtn.onclick = () => {
+        this.drawCard();
+        console.log("clicking")
+      }
+    }
+
+    shuffleDeck() {
+      this.removedDeck.forEach(card => this.cardsDeck.push(card))
+    }
+
+    startGame() {
+    // TEMPORARY - BACKLOG ADD A STARTING SCREEN WITH STORY?
+      this.playerBet()
     }
 
 }
-
-
 
 // Event listener
 
 window.onload = () => {
     startBtn.onclick = () => {
       splashScreen.style.display = "none";
-      gameScreen.style.display = "flex";
-      
-      // animate();
-      // TESTING
+      betScreen.style.display = "flex"
       const newGame = new Game
       newGame.startGame();
     };
